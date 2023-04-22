baf29174f60a0d581b882a8f1a4775310b04cf95
JayPeaa <john.paul.hay@outlook.com>
2023-04-22 12:08:33 +0100

    Fixed security issues and added curd functionality

--- movies/views.py
+++ movies/views.py
@@ -1,28 +1,38 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from .models import Movie
 
-data = {
-    'movies': [
-      {
-        'id':5,
-        'title': 'Jaws',
-        'year': 1979
-      },
-      {
-        'id':6,
-        'title': 'Sharknado',
-        'year': 1999
-      },
-      {
-        'id':7,
-        'title': 'The Meg',
-        'year': 2015
-      }
-    ]
-}
 
 def movies(request):
-    return render(request, 'movies/movies.html', data)
+    data = Movie.objects.all()
+    return render(request, 'movies/movies.html', {'movies': data})
+
 
 def home(request):
     return HttpResponse("This is the Homepage")
+
+
+def detail(request, id):
+    data = Movie.objects.get(pk=id)
+    return render(request, 'movies/detail.html', {'movie': data})
+
+
+def add(request):
+    title = request.POST.get('title')
+    year = request.POST.get('year')
+
+    if title and year:
+        movie = Movie(title=title, year=year)
+        movie.save()
+        return HttpResponseRedirect('/movies')
+
+    return render(request, 'movies/add.html')
+
+
+def delete(request, id):
+    try:
+        movie = Movie.objects.get(pk=id)
+    except:
+        raise Http404('Movie does not exist')
+    movie.delete()
+    return HttpResponseRedirect('/movies')
